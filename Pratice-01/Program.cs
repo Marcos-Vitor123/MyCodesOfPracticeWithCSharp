2f5624456309156458530690f556f0f0c50cf3c2
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-17 02:39:45 -0300

    Prática 01 - Função

---
+++ Pratice-01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Funcao
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FuncaoSemRetorno();
+            int resultado = FuncaoComRetorno(1, 3);
+            Console.WriteLine(resultado);
+        }
+
+        public static void FuncaoSemRetorno()
+        {
+            int a = 10, 
+                b = 4,
+                resultado = a + b;
+
+            Console.WriteLine(resultado);
+        }
+
+        public static int FuncaoComRetorno(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# My Codes of Practice With CSharp
+
+Here you will find my codes practiced with CSharp, regardless of any course. Here is the hands-on time, where I challenge myself to create any small functionality in order to better retain the content acquired in courses and documentation.
